8bf6c5381725a9b279135d65c68785393c372787
Nordine Bittich <nordine@bittich.be>
2022-03-30 22:04:17 +0200

    add docker script

---
+++ artemis-docker/push-docker-image.sh
@@ -0,0 +1,11 @@
+# bash prepare-docker.sh --from-release --artemis-version 2.20.0            
+# cd _TMP_/artemis/2.20.0                                                   
+
+set -e
+ARTEMIS_VERSION=$1 # 2.21.0
+ARTCODED_VERSION=$2 # 2022.0.0
+bash prepare-docker.sh --from-release --artemis-version $ARTEMIS_VERSION     
+cd ./_TMP_/artemis/$ARTEMIS_VERSION                                                            
+docker build -f ./docker/Dockerfile-adoptopenjdk-11 -t artcoded/artemis:v$ARTCODED_VERSION .                                                                   
+docker tag artcoded/artemis:v$ARTCODED_VERSION artcoded:5000/artcoded/artemis:v$ARTCODED_VERSION                                                                         
+docker push artcoded:5000/artcoded/artemis:v$ARTCODED_VERSION      
