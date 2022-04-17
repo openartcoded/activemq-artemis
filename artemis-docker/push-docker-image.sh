512f7083cb9169ab409729afd9a5867f38e99898
Nordine Bittich <nordine@bittich.be>
2022-04-17 08:01:24 +0200

    upgrade java

--- artemis-docker/push-docker-image.sh
+++ artemis-docker/push-docker-image.sh
@@ -3,9 +3,11 @@
 
 set -e
 ARTEMIS_VERSION=$1 # 2.21.0
-ARTCODED_VERSION=$2 # 2022.0.0
+ARTCODED_VERSION=$2 # 2022.0.1-2_21_0-jre-17
 bash prepare-docker.sh --from-release --artemis-version $ARTEMIS_VERSION     
 cd ./_TMP_/artemis/$ARTEMIS_VERSION                                                            
-docker build -f ./docker/Dockerfile-adoptopenjdk-11 -t artcoded/artemis:v$ARTCODED_VERSION .                                                                   
-docker tag artcoded/artemis:v$ARTCODED_VERSION nbittich/artemis:v$ARTCODED_VERSION                                                                         
-docker push nbittich/artemis:v$ARTCODED_VERSION      
+docker build -f ./docker/Dockerfile-openjdk-17 -t nbittich/artemis:v$ARTCODED_VERSION .                                                                   
+docker tag nbittich/artemis:v$ARTCODED_VERSION nbittich/artemis:v$ARTCODED_VERSION                                                                         
+docker push nbittich/artemis:v$ARTCODED_VERSION   
+cd -
+rm -rf _TMP_/   
