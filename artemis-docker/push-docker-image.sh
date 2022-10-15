f2f4e99b64a49389fbaf8040bc682335cffd3a64
Nordine Bittich <nordine@bittich.be>
2022-10-15 13:41:30 +0200

    update artemis & jre

--- artemis-docker/push-docker-image.sh
+++ artemis-docker/push-docker-image.sh
@@ -2,11 +2,11 @@
 # cd _TMP_/artemis/2.20.0                                                   
 
 set -e
-ARTEMIS_VERSION=$1 # 2.24.0
-ARTCODED_VERSION=$2 # 2022.1.2-2_24_0-jre-17
+ARTEMIS_VERSION=$1 # 2.26.0
+ARTCODED_VERSION=$2 # 2022.1.2-2_26_0-jre-18
 bash prepare-docker.sh --from-release --artemis-version $ARTEMIS_VERSION     
 cd ./_TMP_/artemis/$ARTEMIS_VERSION                                                            
-docker build -f ./docker/Dockerfile-openjdk-17 -t nbittich/artemis:v$ARTCODED_VERSION .                                                                   
+docker build -f ./docker/Dockerfile-openjdk-18 -t nbittich/artemis:v$ARTCODED_VERSION .                                                                   
 docker tag nbittich/artemis:v$ARTCODED_VERSION nbittich/artemis:v$ARTCODED_VERSION                                                                         
 docker push nbittich/artemis:v$ARTCODED_VERSION   
 cd -
