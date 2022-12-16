528de5655876ebce71884971626ff1e088017a73
Nordine Bittich <nordine@bittich.be>
2022-12-16 21:21:21 +0100

    2023.0.0

--- artemis-docker/push-docker-image.sh
+++ artemis-docker/push-docker-image.sh
@@ -2,8 +2,8 @@
 # cd _TMP_/artemis/2.20.0                                                   
 
 set -e
-ARTEMIS_VERSION=$1 # 2.26.0
-ARTCODED_VERSION=$2 # 2022.1.2-2_26_0-jre-18
+ARTEMIS_VERSION=$1 # 2.27.1
+ARTCODED_VERSION=$2 # 2023.0.0-2_27_1-jre-18
 bash prepare-docker.sh --from-release --artemis-version $ARTEMIS_VERSION     
 cd ./_TMP_/artemis/$ARTEMIS_VERSION                                                            
 docker build -f ./docker/Dockerfile-openjdk-18 -t nbittich/artemis:v$ARTCODED_VERSION .                                                                   
