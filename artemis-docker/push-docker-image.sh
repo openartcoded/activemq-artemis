ca707e64a101e9d4ebe568024f56e6cb3903d969
Nordine Bittich <nordine@bittich.be>
2023-10-24 17:46:29 +0200

    autocommit

--- artemis-docker/push-docker-image.sh
+++ artemis-docker/push-docker-image.sh
@@ -2,12 +2,12 @@
 # cd _TMP_/artemis/2.20.0                                                   
 
 set -e
-ARTEMIS_VERSION=$1 # 2.29.0
-ARTCODED_VERSION=$2 # 2023.2.0-2_29_0-jre-20
-KEYCLOAK_VERSION=$3 # 21.1.2
+ARTEMIS_VERSION=$1 # 2.31.0
+ARTCODED_VERSION=$2 # 2023.3.0-2_31_0-jre-21
+KEYCLOAK_VERSION=$3 # 22.0.5
 bash prepare-docker.sh --from-release --artemis-version $ARTEMIS_VERSION     
 cd ./_TMP_/artemis/$ARTEMIS_VERSION                                                            
-docker build --build-arg KEYCLOAK_VERSION=${KEYCLOAK_VERSION} -f ./docker/Dockerfile-openjdk-20 -t nbittich/artemis:v$ARTCODED_VERSION .                                                                   
+docker build --build-arg KEYCLOAK_VERSION=${KEYCLOAK_VERSION} -f ./docker/Dockerfile-openjdk-21 -t nbittich/artemis:v$ARTCODED_VERSION .                                                                   
 docker tag nbittich/artemis:v$ARTCODED_VERSION nbittich/artemis:v$ARTCODED_VERSION                                                                         
 docker push nbittich/artemis:v$ARTCODED_VERSION   
 cd -
