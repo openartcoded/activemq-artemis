c55e673dac94e16635d817836c1c3618f5090e90
Nordine Bittich <contact@bittich.be>
2025-10-02 10:54:33 +0200

    java 25

--- artemis-docker/push-docker-image.sh
+++ artemis-docker/push-docker-image.sh
@@ -1,14 +1,14 @@
-# bash prepare-docker.sh --from-release --artemis-version 2.28.0            
-# cd _TMP_/artemis/2.20.0                                                   
+# bash prepare-docker.sh --from-release --artemis-version 2.28.0
+# cd _TMP_/artemis/2.20.0
 
 set -e
-ARTEMIS_VERSION=$1 # 2.31.2
+ARTEMIS_VERSION=$1  # 2.31.2
 ARTCODED_VERSION=$2 # 2024.0.0-2_31_2-jre-21
 KEYCLOAK_VERSION=$3 # 23.0.1
-bash prepare-docker.sh --from-release --artemis-version $ARTEMIS_VERSION     
-cd ./_TMP_/artemis/$ARTEMIS_VERSION                                                            
-docker build --build-arg KEYCLOAK_VERSION=${KEYCLOAK_VERSION} -f ./docker/Dockerfile-openjdk-21 -t nbittich/artemis:v$ARTCODED_VERSION .                                                                   
-docker tag nbittich/artemis:v$ARTCODED_VERSION nbittich/artemis:v$ARTCODED_VERSION                                                                         
-docker push nbittich/artemis:v$ARTCODED_VERSION   
+bash prepare-docker.sh --from-release --artemis-version $ARTEMIS_VERSION
+cd ./_TMP_/artemis/$ARTEMIS_VERSION
+docker build --build-arg KEYCLOAK_VERSION=${KEYCLOAK_VERSION} -f ./docker/Dockerfile-openjdk-25 -t nbittich/artemis:v$ARTCODED_VERSION .
+docker tag nbittich/artemis:v$ARTCODED_VERSION nbittich/artemis:v$ARTCODED_VERSION
+docker push nbittich/artemis:v$ARTCODED_VERSION
 cd -
-rm -rf _TMP_/   
+rm -rf _TMP_/
